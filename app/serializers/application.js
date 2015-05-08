d7b0e2c186f4e97db7e27cde391a2eeacb55126e
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-05-08 15:57:47 -0300

    Usando jpa-serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,9 +1,9 @@
-import DS from 'ember-data';
+import JpaSerializer from 'ember-jpa-adapter/serializers/jpa';
 
-export default DS.RESTSerializer.extend({
+export default JpaSerializer.extend({
   extractMeta: function(store, type, payload) {
     if (payload && payload.links) {
-      store.setMetadataFor(type, { links: payload.links }); 
+      store.setMetadataFor(type, {links: payload.links}); 
       delete payload.links;
     }
     return this._super(store, type, payload);
