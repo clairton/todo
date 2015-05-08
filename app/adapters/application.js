27e37ae92490c1105a57a6f9eb1dbd6d90ab5f58
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-05-08 15:58:49 -0300

    Usando jpa-adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,9 @@
-import DS from 'ember-data';
+import JpaAdapter from 'ember-jpa-adapter/adapters/jpa';
+import config from '../config/environment';
 
-export default DS.RESTAdapter.extend({
+export default JpaAdapter.extend({
+  namespace: config.namespace,
+  host: config.host,
   buildURL: function(type, id, snapshot) {
     var url = this._super(type, id, snapshot);
     return url+".json";
